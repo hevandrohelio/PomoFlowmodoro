32fa38335fad245af32dcbe024c8a48e8e63ad8b
Hevandro Hélio <68600487+hevandrohelio@users.noreply.github.com>
2026-02-07 15:09:42 -0300

    I think this timer is working a little

--- project/script.js
+++ project/script.js
@@ -1,42 +1,83 @@
 // pomodoro
+let isRunning = false;
+// elements
+const pomodoroStartBtn = document.getElementById("start_btn");
+const pomodoroPauseBtn = document.getElementById("pause_btn");
+let timerMode = 'focus';
+let pomodoroFocusTime = document.getElementById("focus_time").value;
 
-// define pomodoro focus time
-let isPomoStarted = false;
+const timerText = document.getElementById("timer_text");
+timerText.innerHTML = `${pomodoroFocusTime}:00`;
 
-const focus_time = document.getElementById("focus_time").value; // focus time on setting
+let pomodoroBreakTime = document.getElementById("break_time").value;
+let pomodoroLongBreakTime = document.getElementById("long_break_time").value;
 
-const timer_text = document.getElementById("timer_text");
-timer_text.innerHTML  = `${focus_time}:00`;
+let timer = minutesToSeconds(pomodoroFocusTime);
+let clock = null;
 
-let mode = "focus"; // focus | break | long_break
-let focus_sessions = document.getElementById("long_break_interval").value;
-let remainingSeconds = 0;
-
-
-function minutesToSeconds(minutes) {
-  return minutes * 60;
+function minutesToSeconds(t) {
+    return t * 60
 }
 
-function formatTime(seconds) {
-    const min = String(Math.floor(seconds/60)).padStart(2, "0");
-    const sec = String(seconds%60).padStart(2,"0");
-    return `${min}:${sec}`
+function displayTimer(time) {
+    if (time < 600 && time > 10) {
+        (time % 60 == 0) ? timerText.innerHTML = `0${Math.floor(time / 60)}:0${time % 60}` : timerText.innerHTML = `0${Math.floor(time / 60)}:${time % 60}`;
+    }
+    else if (time < 10) {
+        (time % 60 == 0) ? timerText.innerHTML = `0${Math.floor(time / 60)}:0${time % 60}` : timerText.innerHTML = `0${Math.floor(time / 60)}:0${time % 60}`;
+    }
+    else {
+        (time % 60 == 0) ? timerText.innerHTML = `${Math.floor(time / 60)}:0${time % 60}` : timerText.innerHTML = `0${Math.floor(time / 60)}:${time % 60}`;
+    }
 }
 
-function updateDisplay() {
-    timer_text.innerHTML = formatTime(remainingSeconds);
+// modes
+function changeMode(mode) {
+    if (mode === 'focus') {
+        timerMode = 'focus';
+        timer = minutesToSeconds(pomodoroFocusTime);
+        displayTimer(timer);
+    }
+    if (mode === 'break') {
+        timerMode = 'break';
+        timer = minutesToSeconds(pomodoroBreakTime);
+        displayTimer(timer);
+    }
+    if (mode === 'long_break') {
+        timerMode = 'long_break';
+        timer = minutesToSeconds(pomodoroLongBreakTime);
+        displayTimer(timer);
+    }
 }
 
+// clock control
+function start() {
+    if (isRunning) return;
+    pomodoroStartBtn.style.display = "none";
+    pomodoroPauseBtn.style.display = "inline";
+    isRunning = true;
+    clock = setInterval(() => {
+        if (timer === 0) {
+            clearInterval(clock);
+            isRunning = false;
+        }
+        timer--;
+        displayTimer(timer);
+    }, 1000);
+}
+function pause() {
+    pomodoroStartBtn.style.display = "inline";
+    pomodoroPauseBtn.style.display = "none";
+    if (!isRunning) return;
 
-
-// start pomodoro
-
-function startFocus() {
-    isPomoStarted = true;
+    clearInterval(clock);
+    isRunning = false;
+}
+function reset() {
+    pomodoroStartBtn.style.display = "inline";
+    pomodoroPauseBtn.style.display = "none";
+    clearInterval(clock);
+    isRunning = false;
+    changeMode(timerMode);
 
 }
-
-
-// pause pomodoro
-
-// reset pomodoro
